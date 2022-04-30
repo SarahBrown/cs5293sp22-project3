b398caf713eb655e51dbe224de1f29220636b946
Sarah Brown <srb@ou.edu>
2022-04-30 01:53:55 -0500

    auto redact data for testing etc

--- project3.py
+++ project3.py
@@ -1,37 +1,73 @@
-import argparse
-import glob
-import sys
-import os
+import pandas as pd
+import csv
+import re
+
+import sklearn
+import nltk
+from nltk.corpus import stopwords
+from sklearn.neural_network import MLPClassifier
+
+def process_sent(df_part):
+    y = df_part.drop(['github', 'name'],axis=1)
+
+    df_part = clean_context(df_part, 'context', 'context_tidy')
+    df_part['redact_len'] = df_part['context'].apply(lambda x: count_redact(x))
+    df_part.to_csv('test.tsv', sep="\t", index=False)
+
+    X = df_part.drop(['github', 'name', 'context'],axis=1)
+    return X, y
+
+def clean_context(df, col, tidy_col):
+    stop = stopwords.words('english')
+    #df[tidy_col] = df[col].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)])) # removes stop words
+    df[tidy_col] = df[col].apply(lambda x: x.replace('\u2588','')) # removes redacted text
+    df[tidy_col] = df[tidy_col].apply(lambda x: re.sub('[^a-zA-Z\'\"]', ' ', x)) # removes punctuation
+    df[tidy_col] = df[tidy_col].apply(lambda x: re.sub('[\'\"]', '', x)) # removes punctuation
+    df[tidy_col] = df[tidy_col].apply(lambda x: re.sub(' +', ' ', x)) # removes extra spaces
+
+    return df
+
+def count_redact(str):
+    count = 0
+    for i in str:
+        if (i == '\u2588'):
+            count += 1
+
+    return count
+
+def load_data():
+    data = []
+
+    # loads data from tsv and separates based on data type (train, test, valid)
+    with open('resources/data_handcleaned.tsv') as file:
+        tsv_file = csv.reader(file, delimiter="\t")
+        
+        # printing data line by line
+        for line in tsv_file:
+            data.append(line)
+
+    # turns lists of lists into pandas df
+    data_df = pd.DataFrame(data, columns = ['github', 'type', 'name', 'context'])
+
+    return data_df
 
 def main():
-    # adds arguments, inputfiles, and performs redaction
-    args = add_arguments()
-    input_files = get_inputfiles(args.input)
-    redact(args, input_files)
+    # for training
+    data_df = load_data()
+    X, y = process_sent(data_df)
+
+    X_train = (X.loc[X['type'] == 'training']).values.tolist()
+    y_train = (y.loc[y['type'] == 'training']).values.tolist()
+
+    X_valid = (X.loc[X['type'] == 'validation']).values.tolist()
+    y_valid = (y.loc[y['type'] == 'validation']).values.tolist()
+
+    X_test = (X.loc[X['type'] == 'testing']).values.tolist()
+    y_test = (y.loc[y['type'] == 'testing']).values.tolist()
+
+    clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
+    clf.predict(X_valid)
+    clf.score(X_valid, y_valid)
 
 if __name__ == "__main__":
     main()
-
-def add_arguments():
-    """Function to add and parse arguments."""
-    parser = argparse.ArgumentParser()        
-    # get and return args
-    args = parser.parse_args()
-    return args
-
-def get_inputfiles(input_glob):
-    """Function to process glob and add input files."""
-    files = sorted(glob.glob(input_glob)) # reads in glob files and sorts them alphabetically for ease of debugging
-    file_list = []
-
-    # opens each file in glob and creates a FileStats object to hold its details
-    for f in files:
-        file = open(f, "r") 
-        new_file = FileStats.FileStats(f, file.read()) # creates FileStats obj
-        file_list.append(new_file) # adds to list of files
-        file.close()
-
-    return file_list # returns list
-
-def redact_names_files(args, input_file):
-    
---
+++ project3/makedata.py
@@ -0,0 +1,143 @@
+import argparse
+import glob
+import sys
+import os
+import re
+
+import nltk
+import spacy
+import en_core_web_lg
+import pandas as pd
+
+nlp = en_core_web_lg.load()
+
+def redact_names(input_files, training_set_to_submit):
+    """Function to redact names in input text file and generate test data."""
+    # loops through each input file and finds names
+    df = pd.DataFrame(columns =['github', 'type', 'name', 'context'])
+    
+    if (not training_set_to_submit):
+        num_files = len(input_files)
+        num_train = int(num_files*0.66)
+        num_left = num_files - num_train
+        num_valid = int(num_left*0.66)
+        num_test = num_left-num_valid
+
+        for i in range(0,num_train):
+            inp = re.sub("<br /><br />", " ", input_files[i])
+            redacted = redact_per_sent(inp)
+
+            for redact in redacted:
+                df.loc[len(df.index)] = ['SarahBrown', 'training', redact[0], redact[1]]
+        
+        for i in range(num_train,num_train+num_valid):
+            inp = re.sub("<br /><br />", " ", input_files[i])
+            redacted = redact_per_sent(inp)
+
+            for redact in redacted:
+                df.loc[len(df.index)] = ['SarahBrown', 'validation', redact[0], redact[1]]
+
+        for i in range(num_train+num_valid,num_train+num_valid+num_test):
+            inp = re.sub("<br /><br />", " ", input_files[i])
+            redacted = redact_per_sent(inp)
+            for redact in redacted:
+                df.loc[len(df.index)] = ['SarahBrown', 'testing', redact[0], redact[1]]
+        
+        return df
+
+
+    else:
+        for inp in input_files:
+            inp = re.sub("<br /><br />", " ", inp)
+            redacted = redact_per_sent(inp)
+
+            for redact in redacted:
+                if (len(df.index) < 60):
+                    df.loc[len(df.index)] = ['SarahBrown', 'training', redact[0], redact[1]]
+                elif(len(df.index) < 110):
+                    df.loc[len(df.index)] = ['SarahBrown', 'validation', redact[0], redact[1]]
+                elif(len(df.index) < 130):
+                    df.loc[len(df.index)] = ['SarahBrown', 'testing', redact[0], redact[1]]
+                else:
+                    return df
+         
+def redact_per_sent(inp):
+    """Function to find and redact names in an input sentence."""
+    redacted = []
+    for sent in nlp(inp).sents:
+        doc = nlp(sent.text)
+        f = list(filter(lambda e: e.label_ in ["PERSON"], doc.ents))
+
+        # filters to sentences with only one entity labeled person in them
+        if (len(f) == 1):
+            for ent in doc.ents:
+                if (ent.label_ == "PERSON"):
+                    redact_sent = redact(sent.text, ent.start_char, ent.start_char+len(ent.text)) # sent to redact, start char, end char
+
+                    if (char_len(redact_sent) > 1000):
+                        if (ent.start == 0):
+                            redact_sent = redact_sent[:10]
+                        elif (ent.start == len(redact_sent)-1):
+                            redact_sent = redact_sent[10:-1]
+                        else:
+                            redact_sent = redact_sent[ent.start-5, ent.start+5]
+                    
+                    redacted.append([ent.text, redact_sent])
+
+    return redacted
+
+def char_len(text):
+    """Function to count number of characters in a string."""
+    total = 0
+
+    for i in text:
+        total += 1
+    
+    return total
+
+def redact(text, start_char, end_char):
+    """Function to replace character array with \u2588 characters."""
+    redact_indexes = range(start_char, end_char)
+    redacted = text
+
+    for index in redact_indexes:
+        redacted = redacted[:index] + "\u2588" + redacted[index+1:]
+
+    return redacted
+
+def add_arguments():
+    """Function to add and parse arguments."""
+    parser = argparse.ArgumentParser()     
+    parser.add_argument("--input", type=str, required=True, 
+                            help="Glob of local files stored in resources folder.")
+   
+    # get and return args
+    args = parser.parse_args()
+    return args
+
+def get_inputfiles(input_glob):
+    """Function to process glob and add input files."""
+    files = sorted(glob.glob(input_glob)) # reads in glob files and sorts them alphabetically for ease of debugging
+    file_list = []
+
+    # opens each file in glob and adds text of file to a list
+    for f in files:
+        file = open(f, "r") 
+        file_list.append(file.read()) # text to list
+        file.close()
+
+    return file_list # returns list
+
+def main():
+    # adds arguments, inputfiles, and performs redaction
+    args = add_arguments()
+    input_files = get_inputfiles(args.input)
+
+    training_set_to_submit = False
+
+    df = redact_names(input_files, training_set_to_submit)
+    print(f'Number of training: {len(df[df["type"] == "training"])}.\nNumber of valid: {len(df[df["type"] == "validation"])}.\nNumber of testing: {len(df[df["type"] == "testing"])}.')
+    df.to_csv('resources/data.tsv', sep="\t", index=False)
+
+if __name__ == "__main__":
+    main()
