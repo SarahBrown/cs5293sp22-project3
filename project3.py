caedc5f2a8d7c43fb44ea4f01355840275559ef9
Sarah Brown <srb@ou.edu>
2022-05-02 15:57:06 -0500

    backup

--- project3.py
+++ project3.py
@@ -12,6 +12,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
 import numpy as np
 import spacy
 import en_core_web_lg
@@ -26,7 +29,6 @@ def process_sent(df_part):
     y = df_part.drop(['context'],axis=1)
 
     df_part = clean_context(df_part, 'context', 'context_tidy', True)
-    df_part.to_csv('resources/test.tsv', sep="\t", index=False)
 
     X = df_part.drop(['name', 'context'],axis=1)
     return X, y
@@ -72,26 +74,11 @@ def convert_to_dict_and_len(X_data, y):
     X = X_data.drop(['github', 'type', 'redact_len'],axis=1)
     X_len = X_data.drop(['github', 'type', 'context_tidy'],axis=1)
 
-    X_dict = (X.T.to_dict().values())
-    #print(X_dict)
-    vect = DictVectorizer(sparse=True)
-    formatted_X = vect.fit_transform(X_dict)
-    #print(vect.get_feature_names_out())
-
     X_out = X_data.drop(['github', 'type'],axis=1)
     X_out = X_out.to_numpy().flatten()
     
     arr = X.to_numpy().flatten()
     formatted_X = arr
-
-
-    # vectorizer = CountVectorizer()
-    # corpus = arr
-    # tokenizer = vectorizer.build_tokenizer()
-    # vocab = [tokenizer(str) for str in corpus]
-    # vectorizer.fit(corpus)
-
-    # formatted_vec_X = vectorizer.transform(corpus)
 
     arr = X_len.to_numpy()
     formatted_X_len = arr
@@ -102,24 +89,21 @@ def convert_to_dict_and_len(X_data, y):
 
     return formatted_X, formatted_X_len, formatted_y
 
-def train_model(X, X_len, y):
+def train_model(X , y):
     print("training")
-    vectorizer = CountVectorizer()
-    vectorizer.fit(X)
-    X_vec = vectorizer.transform(X)
-    X_vec_arr = X_vec.toarray()
-    arr = np.concatenate((X_vec_arr,X_len),axis=1)
     #clf = MLPClassifier(random_state=1, max_iter=500).fit(X_vec, y)
     #clf = LogisticRegression(solver='liblinear').fit(arr, y)
-    clf = make_pipeline(TfidfVectorizer(max_features = 50000,smooth_idf=True), MLPClassifier(random_state=1, max_iter=500))
     #clf = make_pipeline(CountVectorizer(), MLPClassifier(random_state=1, max_iter=500))
-    #clf.fit(X_vec,y)
+    # clf = make_pipeline(CountVectorizer(), MLPClassifier(random_state=1, max_iter=500))
+    
+    clf = MLPClassifier(random_state=1, max_iter=50)
+    clf.fit(X,y)
 
     print("trained")
     with open('model.pkl','wb') as f:
         pickle.dump(clf,f)
 
-    print(clf.score(X_vec,y))
+    print(clf.score(X,y))
 
 
 def load_model():
@@ -132,52 +116,84 @@ def concat_len(arr, len_arr):
     concat_arr = np.concatenate((arr,len_arr),axis=1)
     return concat_arr
 
-
 def main():
     # load and convert data
     data_df = load_data()
     X, y = process_sent(data_df)
 
-    X_train = (X.loc[X['type'] == 'training'])
-    y_train = (y.loc[y['type'] == 'training'])
+    # X_train = (X.loc[X['type'] == 'training'])
+    # y_train = (y.loc[y['type'] == 'training'])
 
-    X_train, X_train_len, y_train = convert_to_dict_and_len(X_train, y_train)
+    # X_train, X_train_len, y_train = convert_to_dict_and_len(X_train, y_train)
 
-    X_valid = (X.loc[X['type'] == 'validation'])
-    y_valid = (y.loc[y['type'] == 'validation'])
-    X_valid, X_valid_len, y_valid = convert_to_dict_and_len(X_valid, y_valid)
+    # X_valid = (X.loc[X['type'] == 'validation'])
+    # y_valid = (y.loc[y['type'] == 'validation'])
+    # X_valid, X_valid_len, y_valid = convert_to_dict_and_len(X_valid, y_valid)
 
-    X_test = (X.loc[X['type'] == 'testing'])
-    y_test = (y.loc[y['type'] == 'testing'])
-    X_test, X_test_len, y_test = convert_to_dict_and_len(X_test, y_test)
+    # X_test = (X.loc[X['type'] == 'testing'])
+    # y_test = (y.loc[y['type'] == 'testing'])
+    # X_test, X_test_len, y_test = convert_to_dict_and_len(X_test, y_test)
 
-    vectorizer = CountVectorizer()
-    vectorizer.fit(X_train)
+    # vectorizer = CountVectorizer()
+    # vectorizer.fit(X_train)
 
-    X_vec_valid = vectorizer.transform(X_valid)
-    X_arr_valid = X_vec_valid.toarray()
-    X_valid_arr = np.concatenate((X_arr_valid,X_valid_len),axis=1)
+    # X_vec_train = vectorizer.transform(X_train)
+    # X_train_arr = concat_len(X_vec_train.toarray(), X_train_len)
+    
+    # X_vec_valid = vectorizer.transform(X_valid)
+    # X_valid_arr = concat_len(X_vec_valid.toarray(), X_valid_len)
 
-
-    X_vec_test = vectorizer.transform(X_test)
-    X_arr_test = X_vec_test.toarray()
-    X_test_arr = np.concatenate((X_arr_test,X_test_len),axis=1)
-
+    # X_vec_test = vectorizer.transform(X_test)
+    # X_test_arr = concat_len(X_vec_test.toarray(), X_test_len)
 
     # # trains and saves model
-    # train_model(X_train, X_train_len, y_train)
+    # train_model(X_train_arr, y_train)
 
     # # loads model
-    clf = load_model()
-    print(clf.score(X_vec_valid,y_valid))
-    print(clf.score(X_vec_test,y_test))
-    predicts = clf.predict(X_vec_valid)
-    for i in range(len(predicts)):
-        if ((X_valid[i] == y_valid[i])): #(y_valid[i] in y_train) and 
-            print("Valid:",i)
-            print(f'{X_valid[i]}\n{y_valid[i]}\n{predicts[i]}')
-            #print(f'Was X_valid[i] in test? {X_valid[i] in X_train}')
+    # clf = load_model()
 
+    # print(clf.score(X_valid_arr,y_valid))
+    # print(clf.score(X_test_arr,y_test))
+
+    # predicts = clf.predict(X_vec_valid)
+    # for i in range(len(predicts)):
+    #     if ((X_valid[i] == y_valid[i])): #(y_valid[i] in y_train) and 
+    #         print("Valid:",i)
+    #         print(f'{X_valid[i]}\n{y_valid[i]}\n{predicts[i]}')
+    #         #print(f'Was X_valid[i] in test? {X_valid[i] in X_train}')
+
+
+    df_selected = X.drop(['github', 'type', 'redact_len'], axis=1)
+    df_features = df_selected.to_dict(orient='records')
+    print(df_features)
+    vectorizer = DictVectorizer()
+    features = vectorizer.fit_transform(df_features).toarray()
+
+    labels = y.drop(['github', 'type'],axis=1).to_numpy().flatten()
+
+    # features_train, features_test, labels_train, labels_test = train_test_split(
+    #     features, labels, 
+    #     test_size=0.20, random_state=42)
+
+    # train_model(features_train, labels_train)
+
+    # clf = load_model()
+    # print(clf.score(features_test,labels_test))
+    # predicts = clf.predict(features_test)
+
+    # labels = list(labels_train.flatten())
+    # print("Correct")
+    # for i in range(len(predicts)):
+    #     if (labels_test[i] == predicts[i]):
+    #         count = labels.count(labels_test[i])
+    #         print(f'Expected: {labels_test[i]}. Actual: {predicts[i]}. Count in train: {count}')
+    # print("Incorrect")
+    # for i in range(len(predicts)):
+    #     if (labels_test[i] != predicts[i]):
+    #         count = labels.count(labels_test[i])
+    #         print(f'Label: {labels_test[i]}. Model: {predicts[i]}. Count in train: {count}')
+
+    # TODO Unredacted texts should go in the output folder location.
 
 if __name__ == "__main__":
     main()
--- project3/makedata.py
+++ project3/makedata.py
@@ -49,7 +49,7 @@ def redact_names(input_files, training_set_to_submit):
                 df.loc[len(df.index)] = ['SarahBrown', 'training', redacted[i][0], redacted[i][1]]
             
             if (input%100 == 0):
-                print(f'{input}/{num_valid}')
+                print(f'{input}/{num_train+num_valid}')
 
         print(f'{num_train+num_valid} files done.')
 
@@ -61,7 +61,7 @@ def redact_names(input_files, training_set_to_submit):
                 df.loc[len(df.index)] = ['SarahBrown', 'training', redacted[i][0], redacted[i][1]]
 
             if (input%100 == 0):
-                print(f'{input}/{num_test}')
+                print(f'{input}/{num_test+num_train+num_valid}')
 
         
         return df
@@ -95,7 +95,10 @@ def redact_per_sent(inp):
                 if (ent.label_ == "PERSON"):
                     redact_sent = redact(sent.text, ent.start_char, ent.start_char+len(ent.text)) # sent to redact, start char, end char
 
-                    if (char_len(redact_sent) > 1000):
+                    if (char_len(redact_sent) < 100):
+                        break
+
+                    elif (char_len(redact_sent) > 1000):
                         if (ent.start == 0):
                             redact_sent = redact_sent[:10]
                         elif (ent.start == len(redact_sent)-1):
